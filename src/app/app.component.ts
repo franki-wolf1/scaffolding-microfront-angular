bbecbd44f4ffa9e3fb86315b28a14b874bfacdfe
interruta <interruta.sistincon@gmail.com>
2024-03-25 16:36:56 -0500

    Uso de componentes core Angular 17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core'; 
+import { LayoutComponent } from './core/components/layout/layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [LayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
